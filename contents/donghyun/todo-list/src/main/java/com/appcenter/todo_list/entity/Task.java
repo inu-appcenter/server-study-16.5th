a028eeaa9132b56d7192245906318e8a6ff469d6
Teach-D <wkadht0619@gmail.com>
2024-10-13 10:58:40 +0900

    [Feat] entity, repository, erd, db연결

---
+++ contents/donghyun/todo-list/src/main/java/com/appcenter/todo_list/entity/Task.java
@@ -0,0 +1,26 @@
+package com.appcenter.todo_list.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ contents/donghyun/todo-list/src/main/java/com/appcenter/todo_list/entity/User.java
@@ -0,0 +1,26 @@
+package com.appcenter.todo_list.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+}
