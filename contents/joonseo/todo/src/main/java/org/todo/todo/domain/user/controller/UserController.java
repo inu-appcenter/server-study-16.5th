f70724a893de44a606dab8d27debedd48b355798
xunxxoie <ahh0520@naver.com>
2024-11-04 03:38:52 +0900

    [Feat] todo CRUD - #9

--- contents/joonseo/todo/src/main/java/org/todo/todo/domain/user/controller/UserController.java
+++ contents/joonseo/todo/src/main/java/org/todo/todo/domain/user/controller/UserController.java
@@ -1,27 +1,27 @@
-//package org.todo.todo.domain.user.controller;
-//
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
-//import io.swagger.v3.oas.annotations.responses.ApiResponses;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.todo.todo.domain.todo.dto.res.ResponseDto;
-//
-//@Slf4j
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/users")
-//public class UserController {
-//
-//    @Operation(summary = "유저 생성")
-//    @ApiResponses({
-//            @ApiResponse(responseCode = "201", description = "유저 생성 성공"),
-//            @ApiResponse(responseCode = "400", description = "중복 이메일 존재")
-//    })
-//    @PostMapping
-//    public ResponseEntity<ResponseDto<Long>> createUser()
-//}
+package org.todo.todo.domain.user.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.todo.todo.domain.todo.dto.res.ResponseDto;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Operation(summary = "유저 생성")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "유저 생성 성공"),
+            @ApiResponse(responseCode = "400", description = "중복 이메일 존재")
+    })
+    @PostMapping
+    public ResponseEntity<ResponseDto<Long>> createUser()
+}
